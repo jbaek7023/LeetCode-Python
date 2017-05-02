4f1080dd39abbe46860512d1cdff56f669d3f81e
Jae min Baek <jbaek7023@gmail.com>
2017-05-02 14:13:33 +0900

    448

---
+++ 448FindAllNumbersDisappearedinanArray.py
@@ -0,0 +1,30 @@
+import unittest
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # store index element
+        for i in xrange(len(nums)):
+            # 3 1 1
+            # index : 2 0 0
+            index = abs(nums[i]) - 1
+            nums[index] = - abs(nums[index])
+            # -3 1 -1
+        return [ i+1 for i in xrange(len(nums)) if nums[i] > 0 ]
+
+class SolutionUnitTest(unittest.TestCase):
+    def testArray(self):
+        s = Solution()
+        input = [4,3,2,7,8,2,3,1]
+        output = [5,6]
+        self.assertEqual(s.findDisappearedNumbers(input), output)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# Note
+# xrange and range are the exact same in terms of functionality
+# only difference is that xrange returns List
+# range returns object
---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode Python Algorithm
+
+### Completed
+448. Find All Numbers Disappeared In Array
+
